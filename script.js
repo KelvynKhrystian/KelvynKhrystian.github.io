ae62034af89d754f34dcb81efdddfb70ed16c104
KelvynKhrystian <kelvynkhrystian@pop-os.localdomain>
2022-03-14 16:58:49 -0300

    ok

---
+++ script.js
@@ -0,0 +1,9 @@
+//criação do menu mobile
+let btnMenu = document.getElementById("btn");
+
+
+btnMenu.addEventListener ("click", function (){
+    let menu = document.getElementById("menu");
+    menu.classList.toggle("apareceMenu");
+
+})
