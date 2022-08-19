532f3148ae80aec8b8241a9c661632fde710025a
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2022-08-19 19:08:01 -0300

    apagando-arquivos-2

--- script.js
+++
@@ -1,9 +0,0 @@
-//criação do menu mobile
-let btnMenu = document.getElementById("btn");
-
-
-btnMenu.addEventListener ("click", function (){
-    let menu = document.getElementById("menu");
-    menu.classList.toggle("apareceMenu");
-
-})
