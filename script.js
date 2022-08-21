0e3c17735c31c95de9d6fe560959836ff8b0cac5
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2022-08-21 17:03:32 -0300

    header-size

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ btnMenu.addEventListener ("click", function (){
   menu.classList.toggle("menu-flex");
   // console.log(menu.className);
 });
+
+// Imagem Perfil 3D - Programador BR
+// Fiz mudanças no css por causa do conflito de centralização com outros elementos
+// https://www.youtube.com/watch?v=B-ifg3ZuKKE&list=PLIzJhTi3cja7FfbEQ9fBna0m7oP1Lukxa&index=3
+
+const card = document.getElementById("card")
+
+card.addEventListener ("click", function (){
+  card.classList.toggle("flip");
+});
+
